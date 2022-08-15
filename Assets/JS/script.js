3174d8ecc5942e338cd3a59e908430e7e545f24a
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-15 22:00:41 +0300

    addEvent-Listener application

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,12 @@
+let blogLink = document.querySelector(".links")
+   
+ function respond(){
+    alert("Wonderful World of the Feminine")
+ }
+blogLink.addEventListener("click",respond)
+console.log(blogLink)
+
+
 
  
 
